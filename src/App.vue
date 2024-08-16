7a45f2de2272ac68ae77f7398211886518a6245e
yym1623 <yym16233@gmail.com>
2024-08-16 14:21:30 +0900

    ts setting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import maps from './components/maps.vue'
+import maps from '@/components/maps.vue'
+
+
+
+
+// primevue plugins - toast
+// import { useToast } from "primevue/usetoast";
+// const toast = useToast();
+
+// toast ex)
+// toast.add({ severity: 'success', summary: '알림', detail: `총 ${item.length}개 검색되었습니다 `, life: 3000 });
+
 </script>
 
 <template>
   <maps></maps>
+
+  
 </template>
 
 <style lang="scss" scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+
+// 절대 경로 2가지 선택
+// 1. node:url - fileURLToPath
+// 2. path - __dirname
+import { fileURLToPath, URL } from 'node:url';
+// import path from "path";
+
+
+// Vite를 빌드 도구로 사용하지 않는 경우 test구성 파일의 속성을 사용하여 Vitest를 구성할 수 있습니다.
+// import { defineConfig } from 'vitest/config'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    })
+
+  ],
+  resolve: {
+    alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        // '@': path.resolve(__dirname,'./src'),
+    }
+  },
+  // test: {
+  //   globals: true,
+  //   environment: 'happy-dom'
+  // }
 })
